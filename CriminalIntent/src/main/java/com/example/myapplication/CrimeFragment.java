a8726ead38ab339abd8f96e3df89bda1d4bdc9c1
dso753 <dso753@naver.com>
2017-10-10 15:43:45 +0900

    init commit

---
+++ CriminalIntent/src/main/java/com/example/myapplication/CrimeFragment.java
@@ -0,0 +1,92 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.util.UUID;
+
+public class CrimeFragment extends Fragment {
+    private static final String ARG_CRIME_ID = "crime_id";
+
+    private Crime mCrime;
+
+    private EditText mCrimeTitle;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+
+    public static CrimeFragment newInstance(UUID crimeId) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_CRIME_ID, crimeId);
+
+        CrimeFragment fragment = new CrimeFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        mCrime = new Crime();
+        //직접 엑세스
+//        UUID crimeId = (UUID) getActivity().getIntent().getSerializableExtra(CrimeActivity.EXTRA_CRIME_ID);
+        UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
+        mCrime = CrimeLab.getInstance(getActivity()).getCrime(crimeId);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        //위젯 결합
+        mCrimeTitle = (EditText) view.findViewById(R.id.crime_title);
+        mDateButton = (Button) view.findViewById(R.id.crime_date);
+        mSolvedCheckBox = (CheckBox) view.findViewById(R.id.crime_solved);
+
+        //EditText 리스너
+        mCrimeTitle.setText(mCrime.getTitle());
+        mCrimeTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mCrime.setTitle(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        //date 버튼 default 값 지정 및 비활성화
+        mDateButton.setText(DateFormat.getDateInstance(DateFormat.FULL).format(mCrime.getDate()));
+        mDateButton.setEnabled(false);
+
+        //CheckBox 리스터
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                mCrime.setSolved(mSolvedCheckBox.isChecked());
+            }
+        });
+
+        return view;
+    }
+}
---
+++ CriminalIntent/src/main/java/com/example/myapplication/CrimeListActivity.java
@@ -0,0 +1,10 @@
+package com.example.myapplication;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
